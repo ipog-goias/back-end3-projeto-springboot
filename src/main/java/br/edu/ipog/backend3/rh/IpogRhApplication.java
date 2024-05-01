bc0905e8cbe1f68d534f1d1554256b0d0b6def17
Bruno Urbano <brunourb@hotmail.com>
2024-05-01 00:05:45 -0300

    first commit

---
+++ src/main/java/br/edu/ipog/backend3/rh/IpogRhApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ipog.backend3.rh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpogRhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpogRhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ipog/backend3/rh/IpogRhApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ipog.backend3.rh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpogRhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
